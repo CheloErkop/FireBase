21a97cf671159b9029aa9f966c21a73ebafbb577
Chelo <m.pena13@alumnos.santotomas.cl>
2022-11-28 03:05:06 -0300

    Update MainCrearMascota.java

--- app/src/main/java/com/example/firebase/MainCrearMascota.java
+++ app/src/main/java/com/example/firebase/MainCrearMascota.java
@@ -1,5 +1,6 @@
 package com.example.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,28 +47,32 @@ public class MainCrearMascota extends AppCompatActivity {
                 if (n.isEmpty()&& e.isEmpty()&& c.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Ingresar los datos", Toast.LENGTH_SHORT).show();
                 }else{
-                    Map <String, Object> map = new HashMap<>();
-                    map.put("nombre", nombre.getText().toString());
-                    map.put("edad", edad.getText().toString());
-                    map.put("color",color.getText().toString());
-
-
-                    mFirestore.collection("mascotas").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Toast.makeText(getApplicationContext(), "Creado Exitosamente", Toast.LENGTH_SHORT).show();
-                            finish();
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(getApplicationContext(), "Error al Ingresar", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+                    postPet (n,e,c);
                 }
             }
         });
 
+    }
+
+    private void postPet(String n, String e, String c) {
+        Map <String, Object> map = new HashMap<>();
+        map.put("nombre", nombre.getText().toString());
+        map.put("edad", edad.getText().toString());
+        map.put("color",color.getText().toString());
+
+
+        mFirestore.collection("mascotas").add(map).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(), "Creado Exitosamente", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Error al Ingresar", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
